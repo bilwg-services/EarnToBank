df55479a81c7e36695ebca72f44a2387e1ad9f88
kartik patel <patelkartik1910@gmail.com>
2019-01-31 07:49:03 +0530

    Update: HomeActivity
    Add code for navigation view

--- app/src/main/java/com/deucate/earntobank/HomeActivity.kt
+++ app/src/main/java/com/deucate/earntobank/HomeActivity.kt
@@ -8,16 +8,26 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.google.firebase.auth.FirebaseAuth
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.app_bar_home.*
 import kotlinx.android.synthetic.main.nav_header_home.view.*
+import android.content.ActivityNotFoundException
+import android.net.Uri
 
 
 class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private val auth = FirebaseAuth.getInstance()
+    private val util = Util(this)
+
+    private val currentFragment = MutableLiveData<Fragment>()
+    private var currentFragmentID = 8080
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +35,13 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setSupportActionBar(toolbar)
 
         //Navigation Bat
-        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
         nav_view.setNavigationItemSelectedListener(this)
@@ -34,6 +50,15 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         titleTv.text = auth.currentUser!!.displayName
         subTitleTv.text = auth.currentUser!!.email
         Picasso.get().load(auth.currentUser!!.photoUrl).centerCrop().into(nav_view.profilePictureIV)
+
+        currentFragment.observe(this, Observer { rootIt ->
+            rootIt?.let {
+                if (it.id != currentFragmentID) {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, it, null)
+                    currentFragmentID = it.id
+                }
+            }
+        })
 
     }
 
@@ -49,9 +74,57 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         when (item.itemId) {
 
             R.id.nav_logout -> {
-                auth.signOut()
-                startActivity(Intent(this@HomeActivity, LoginActivity::class.java))
-                finish()
+                AlertDialog.Builder(this).setTitle("Warning!!")
+                    .setMessage("Are you sure you want to logout from this app?").setPositiveButton("YES") { _, _ ->
+                        auth.signOut()
+                        startActivity(Intent(this@HomeActivity, LoginActivity::class.java))
+                        finish()
+                    }
+            }
+
+            R.id.nav_share -> {
+                val shareBody = "I found amazing app. Download to earn money"
+                val sharingIntent = Intent(android.content.Intent.ACTION_SEND)
+                sharingIntent.type = "text/plain"
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here")
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody)
+                startActivity(Intent.createChooser(sharingIntent, "Share With"))
+            }
+
+            R.id.nav_rate_us -> {
+                val uri = Uri.parse("market://details?id=$packageName")
+                val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_NEW_DOCUMENT or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+                try {
+                    startActivity(goToMarket)
+                } catch (e: ActivityNotFoundException) {
+                    startActivity(
+                        Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=$packageName")
+                        )
+                    )
+                }
+
+            }
+
+            R.id.nav_task -> {
+                currentFragment.value = TaskFragment()
+            }
+            R.id.nav_pocket -> {
+                currentFragment.value = PocketFragment()
+            }
+            R.id.nav_redeem -> {
+                currentFragment.value = RedeemFragment()
+            }
+            R.id.nav_history -> {
+                currentFragment.value = HistoryFragment()
+            }
+            R.id.nav_group -> {
+                currentFragment.value = GroupFragment()
+            }
+            R.id.nav_alert -> {
+                currentFragment.value = AlertFragment()
             }
 
         }
