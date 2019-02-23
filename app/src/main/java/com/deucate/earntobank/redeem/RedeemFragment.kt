584f5bde76b8be44f55cc42391ca83a960bae654
kartik patel <patelkartik1910>
2019-02-23 05:37:57 +0530

    Add: Redeem code

--- app/src/main/java/com/deucate/earntobank/redeem/RedeemFragment.kt
+++ app/src/main/java/com/deucate/earntobank/redeem/RedeemFragment.kt
@@ -1,7 +1,10 @@
+@file:Suppress("DEPRECATION")
+
 package com.deucate.earntobank.redeem
 
-
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.ProgressDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,12 +13,19 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.deucate.earntobank.HomeActivity
 import com.deucate.earntobank.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_redeem.view.*
 
 class RedeemFragment : Fragment() {
 
     private val pointsPerRupee = HomeActivity.pointsPerRupee
     private val totalPoints = HomeActivity.totalPoints
+
+    private val db = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
+
+    private lateinit var processDialog: AlertDialog
 
     @SuppressLint("SetTextI18n")
     override fun onCreateView(
@@ -25,6 +35,8 @@ class RedeemFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_redeem, container, false)
 
+        processDialog = ProgressDialog.show(activity, "Loading", "Your request is in process")
+
         val totalEarningInRupee = totalPoints / pointsPerRupee
 
         rootView.redeemTotalAmount.text = "₹ $totalEarningInRupee"
@@ -33,10 +45,68 @@ class RedeemFragment : Fragment() {
             if (totalEarningInRupee <= 100) {
                 Toast.makeText(activity, "Reach ₹100 to withdraw money.", Toast.LENGTH_LONG).show()
                 return@setOnClickListener
+            } else {
+                val mobileNumber = rootView.redeemMobileNumber.text.toString()
+                val amount = rootView.redeemAmountToRedeem.text.toString()
+
+                if (mobileNumber.isEmpty()) {
+                    rootView.redeemMobileNumber.error = "Please enter your mobile number"
+                    return@setOnClickListener
+                }
+
+                if (amount.isEmpty()) {
+                    rootView.redeemAmountToRedeem.error = "Please enter amount to redeem"
+                    return@setOnClickListener
+                }
+
+                val amountInLong = amount.toLong()
+
+                if (amountInLong < 100) {
+                    rootView.redeemAmountToRedeem.error = "Minimum withdrawal limit is 100 RS."
+                    return@setOnClickListener
+                }
+
+                sendWithdrawalRequest(mobileNumber, amountInLong)
             }
         }
 
         return rootView
+    }
+
+    private fun sendWithdrawalRequest(mobileNumber: String, amountInLong: Long) {
+        processDialog.show()
+        val data = HashMap<String, Any>()
+        data["MobileNumber"] = mobileNumber
+        data["Amount"] = amountInLong
+        data["UID"] = auth.uid!!
+
+        db.collection(getString(R.string.withdrawel)).document().set(data).addOnCompleteListener {
+            if (it.isSuccessful) {
+                updateUserData(amountInLong)
+            } else {
+                processDialog.dismiss()
+                AlertDialog.Builder(activity).setTitle("Error")
+                    .setMessage(it.exception!!.localizedMessage).show()
+            }
+        }
+    }
+
+    private fun updateUserData(amount: Long) {
+        val data = HashMap<String, Any>()
+        data["TotalPoints"] = totalPoints - amount
+
+        db.collection(getString(R.string.users)).document(auth.uid!!).update(data)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    processDialog.show()
+                } else {
+                    processDialog.dismiss()
+                    HomeActivity.totalPoints = totalPoints - amount
+                    AlertDialog.Builder(activity).setTitle("Error")
+                        .setMessage(it.exception!!.localizedMessage).show()
+                }
+            }
+
     }
 
 
